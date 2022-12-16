cc8a97eed2f3282cf3abd83bd2f4aa3593c3e184
Yurii Rybachok <drummer2009@ukr.net>
2022-12-16 21:51:28 +0200

    add contacts page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Navbar from "./components/navbar/Navbar"
 import Footer from "./components/footer/Footer"
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Contacts from "./pages/Contacts";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       {/* <Home /> */}
-      <Projects />
+      {/* <Projects /> */}
+      <Contacts />
       <Footer />
     </div>
   );
