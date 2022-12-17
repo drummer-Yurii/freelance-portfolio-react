bf0884fc60d8308b166c9c0b8134decd0fa441b4
Yurii Rybachok <drummer2009@ukr.net>
2022-12-17 11:54:51 +0200

    add page with project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar"
 import Footer from "./components/footer/Footer"
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Project from "./pages/Project";
 import Contacts from "./pages/Contacts";
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
     <div className="App">
       <Navbar />
       {/* <Home /> */}
-      <Projects />
+      {/* <Projects /> */}
+      <Project />
       {/* <Contacts /> */}
       <Footer />
     </div>
